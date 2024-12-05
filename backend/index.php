8da366130b392eee6a12da707979a386d910205a
Ivonne <ivonne.perezf@alumno.buap.mx>
2024-12-04 23:30:15 -0600

    Creacion de vendor rest

--- backend/index.php
+++ backend/index.php
@@ -2,31 +2,35 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+
 require __DIR__ . '/vendor/autoload.php';
+
+// Crear la aplicación Slim
 $app = AppFactory::create();
 
-// Establecer la base URL de la aplicación si es necesario
-$app->setBasePath('http://localhost/proyecto/backend/');
+$app->setBasePath('/proyecto/backend');
 
-$app->get('/test', function ($request, $response, $args) {
+// Ruta de prueba
+$app->get('/test', function (Request $request, Response $response, $args) {
     $response->getBody()->write('Test funcionando!');
     return $response;
 });
-// Ruta principal para comprobar que el API funciona
-$app->get('/', function ($request, $response, $args) {
+
+// Ruta principal
+$app->get('/', function (Request $request, Response $response, $args) {
     $data = ['message' => 'Bienvenido al API RESTful'];
     $response->getBody()->write(json_encode($data));
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->get('/answers', function ($request, $response, $args) {
+// Ruta para obtener respuestas
+$app->get('/answers', function (Request $request, Response $response, $args) {
     $form = new Read('paginaods');
     $form->listAnswers();
     $response->getBody()->write(json_encode($form->getData()));
     return $response->withHeader('Content-Type', 'application/json');
 });
 
-$app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+// Ejecutar la aplicación
 $app->run();
 ?>
--- backend/vendor/composer/autoload_static.php
+++ backend/vendor/composer/autoload_static.php
@@ -8,44 +8,73 @@ class ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf
 {
     public static $files = array (
         '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Slim\\Psr7\\' => 10,
             'Slim\\' => 5,
         ),
         'P' => 
         array (
+            'Psr\\Log\\' => 8,
+            'Psr\\Http\\Server\\' => 16,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
         ),
-        'I' => 
+        'N' => 
         array (
-            'Interop\\Container\\' => 18,
+            'Nyholm\\Psr7\\' => 12,
         ),
         'F' => 
         array (
+            'Fig\\Http\\Message\\' => 17,
             'FastRoute\\' => 10,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Slim\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/slim/psr7/src',
+        ),
         'Slim\\' => 
         array (
             0 => __DIR__ . '/..' . '/slim/slim/Slim',
         ),
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/src',
+        ),
+        'Psr\\Http\\Server\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-server-handler/src',
+            1 => __DIR__ . '/..' . '/psr/http-server-middleware/src',
+        ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
-        'Interop\\Container\\' => 
+        'Nyholm\\Psr7\\' => 
         array (
-            0 => __DIR__ . '/..' . '/container-interop/container-interop/src/Interop/Container',
+            0 => __DIR__ . '/..' . '/nyholm/psr7/src',
+        ),
+        'Fig\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/fig/http-message-util/src',
         ),
         'FastRoute\\' => 
         array (
@@ -53,18 +82,13 @@ class ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf
         ),
     );
 
-    public static $prefixesPsr0 = array (
-        'P' => 
-        array (
-            'Pimple' => 
-            array (
-                0 => __DIR__ . '/..' . '/pimple/pimple/src',
-            ),
-        ),
-    );
-
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -72,7 +96,6 @@ class ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit1ff696401163d3fbe8dab8da572d3ebf::$classMap;
 
         }, null, ClassLoader::class);
